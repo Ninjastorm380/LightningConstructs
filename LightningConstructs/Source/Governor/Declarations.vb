08288a029d0d579aad8e0823b8867648b332eb66
Ninjastorm380 <eytrarp@gmail.com>
2023-01-13 23:46:30 -0500

    organize the files of class `governor`

---
+++ LightningConstructs/Source/Governor/Declarations.vb
@@ -0,0 +1,7 @@
+Public Partial Class Governor
+    Private GovernorWatch As Stopwatch
+    Private DeltaTicks As Long
+    Private BaseDelta As Double
+    Private BaseFrequency As UInt16
+    Private WaitTimespan As TimeSpan
+End Class
